ca5ba0ca8a3856a7d6f06273eefce6e3384ad149
AaronEGH16 <aezegonz@gmail.com>
2023-09-22 20:32:35 +0000

    function with remove all c and C of the strings; update propieties of file

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_str = ''
+    for c in my_string:
+        if c != 'C' and c != 'c':
+            new_str += c
+    my_string = new_str
+    return my_string
