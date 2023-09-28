664d06863ee0cd16800eeb12beafd7e4f03e23f0
AaronEGH16 <aezegonz@gmail.com>
2023-09-28 15:57:06 +0000

    added function to class square

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -4,7 +4,7 @@
 
 class Square:
     """ This class define a Square of size = 'size'
-    validating type and value"""
+    validating type and value; and adding function area"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
