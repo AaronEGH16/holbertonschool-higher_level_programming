d8ae0ba6c2faaee53f40faf72d6ccdc79317ac7c
AaronEGH16 <aezegonz@gmail.com>
2023-09-28 14:08:33 +0000

    initialize a class whit private object using arguments

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" This module define a class Square """
+
+
+class Square:
+    """ This class define a Square of size = 'size' """
+    def __init__(self, size):
+        self.__size = size
+    pass
