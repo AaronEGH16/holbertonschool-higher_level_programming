02f31b885254d636de7e07fcb0965cc723c02b99
AaronEGH16 <aezegonz@gmail.com>
2023-09-28 16:59:37 +0000

    fixed error in call of function setter by __init__

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,11 +6,7 @@ class Square:
     """ This class define a Square of size = 'size'
     validating type and value; and adding function area"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
