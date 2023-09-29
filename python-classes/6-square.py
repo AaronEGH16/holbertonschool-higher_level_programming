422613d40a430dc83d87d32d7b48378eecc76f4f
AaronEGH16 <aezegonz@gmail.com>
2023-09-29 16:22:26 +0000

    function to asign a position to square and print it

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -5,7 +5,7 @@
 class Square:
     """ This class define a Square of size = 'size'
     validating type and value; and adding function area"""
-    def __init__(self, size=0, position=(0,0)):
+    def __init__(self, size=0, position=(0, 0)):
         self.size = size
         self.position = position
 
@@ -30,17 +30,26 @@ class Square:
     @property
     def position(self):
         """ return value of Square position """
-        return self.position
+        return self.__position
 
     @position.setter
     def position(self, value):
         """ Set the value of position passed by arguments """
-        
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
 
     def my_print(self):
         """ print Square of area = 'area' by '#' """
         if self.__size > 0:
+            for _ in range(self.__position[1]):
+                print()
             for _ in range(self.__size):
+                for _ in range(self.__position[0]):
+                    print(" ", end="")
                 for _ in range(self.__size):
                     print("#", end="")
                 print()
