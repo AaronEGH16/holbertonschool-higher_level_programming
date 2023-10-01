13e4a2cba6895441814d54d852974f10c539b424
AaronEGH16 <aezegonz@gmail.com>
2023-10-01 18:06:48 +0000

    restoring

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -5,7 +5,7 @@ get string and print whit indentation
 
 
 def text_indentation(text):
-    r""" print text whit \n after '. or ? or :' """
+    """ print text whit \n after '. or ? or :' """
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
