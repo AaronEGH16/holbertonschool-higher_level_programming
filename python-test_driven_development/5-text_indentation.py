08f208db615a9a30f3d5ba8b9021d0f10aff2c17
AaronEGH16 <aezegonz@gmail.com>
2023-10-01 19:19:25 +0000

    added firewall to multiple spaces for indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -9,6 +9,24 @@ def text_indentation(text):
     if not isinstance(text, str):
         raise TypeError("text must be a string")
 
-    new_text = text.replace(". ", ".\n\n")\
-        .replace("? ", "?\n\n").replace(": ", ":\n\n")
-    print(new_text, end="")
+    new_text = text.replace(". ", ".")\
+        .replace("? ", "?").replace(": ", ":")
+
+    if new_text[0] == ' ':
+        flag = True
+    else:
+        flag = False
+    for ch in new_text:
+        if ch == '?' or ch == '.' or ch == ':':
+            print(ch)
+            print()
+            flag = True
+            continue
+        if flag is False:
+            print(ch, end="")
+            if ch == ' ':
+                flag = True
+        else:
+            if ch != ' ':
+                print(ch, end="")
+                flag = False
