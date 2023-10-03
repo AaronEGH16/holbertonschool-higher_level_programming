a9587c9ebbb3352c582b41b81ce4456b5ee3f4f7
AaronEGH16 <aezegonz@gmail.com>
2023-10-03 14:59:56 +0000

    creation of empty class

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This Module contains definition of rectangle
+"""
+
+
+class Rectangle():
+    """
+    This class contains ojects that define a Rectangle
+    """
+    pass
