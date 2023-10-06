82192a6473ef5741965f4e95c24541e8b14b1624
AaronEGH16 <aezegonz@gmail.com>
2023-10-06 20:02:01 +0000

    added new method to BaseGeometry class

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+this module contains all info, methods and
+functions of class "BaseGeometry"
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry Class
+
+    methods:
+        area()
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
