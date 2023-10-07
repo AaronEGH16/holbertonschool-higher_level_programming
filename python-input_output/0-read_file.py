0f171f66294860d8cdae87beb5153e056be8d2da
AaronEGH16 <aezegonz@gmail.com>
2023-10-07 18:32:24 +0000

    added documentation

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """
-
+this module contains a read file function
 """
 
 
 def read_file(filename=""):
     """
-    
+    function to read a file content and print it in standar output
     """
     with open(filename, "r", encoding="utf-8") as file:
         for f in file.read():
