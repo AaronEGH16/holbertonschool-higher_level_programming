de14eca0c866490e470a3c06b2c8b2c7832f8187
AaronEGH16 <aezegonz@gmail.com>
2023-10-07 18:56:13 +0000

    changed documentation text

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -6,7 +6,7 @@ this module contains write file function
 
 def append_write(filename="", text=""):
     """
-    this function write (append at the end of file)
+    this function write (insert text at the end of file)
     and return number of chars writed
     """
     with open(filename, "a", encoding="utf-8") as file:
