e771da8b35378a96a76dc544c016860f69c4a503
AaronEGH16 <aezegonz@gmail.com>
2023-10-07 15:12:00 +0000

    added new methods to subclass rectangle

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This module contains the Rectangle class,
+This class is inherited from the BaseGeometry class.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    Rectangle class inherited from BaseGeometry class
+
+    methods:
+        __init__
+        area()
+        __str__
+    """
+    def __init__(self, width, height):
+        """
+        this method inicialize and check values of height and width
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """
+        returns area of the Rectangle
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """
+        returns a string representation of Rectangle
+        """
+        return "[{}] {}/{}".format(self.__class__.__name__,
+                                   self.__width, self.__height)
