4200afacc146da68ce61879b113cc9faaa3ea348
AaronEGH16 <aezegonz@gmail.com>
2023-10-09 15:12:31 +0000

    Base class

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+this module contains a
+definition of 'Base Class'
+and their methods and functions
+"""
+
+
+class Base:
+    """
+    Base Class:
+
+    Public attributes:
+        id
+
+    Private attributes:
+        __nb_objects
+
+    Methods:
+        __init__(self, id=None)
+    """
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """
+        This method initializes a base class object
+        and assigns an id (it is always an integer).
+
+        If the id argument is not passed,
+        the new id is the base class object number.
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
