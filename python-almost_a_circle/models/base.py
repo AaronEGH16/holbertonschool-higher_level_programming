97e89c02f339ed97dd7bfbe763c230ab828e3aec
AaronEGH16 <aezegonz@gmail.com>
2023-10-11 16:24:29 +0000

    changed if instance

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -40,6 +40,6 @@ class Base:
         """
         return a JSON string representation of dictionaries
         """
-        if list_dictionaries is None or list_dictionaries == []:
+        if (list_dictionaries is None) or (len(list_dictionaries) <= 0):
             return "[]"
         return json.dumps(list_dictionaries)
