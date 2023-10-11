52783cf0dfd520f2742a90dd8c260e1217235699
AaronEGH16 <aezegonz@gmail.com>
2023-10-11 16:36:35 +0000

    i forgotten add declaration of method type

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -36,10 +36,11 @@ class Base:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
 
+    @staticmethod
     def to_json_string(list_dictionaries):
         """
         return a JSON string representation of dictionaries
         """
-        if list_dictionaries is None:
+        if (list_dictionaries is None) or (len(list_dictionaries) == 0):
             list_dictionaries = []
         return json.dumps(list_dictionaries)
