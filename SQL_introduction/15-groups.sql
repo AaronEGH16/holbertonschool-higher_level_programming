b43e941a7e3c515340100d1cac2669ee923e1ac1
AaronEGH16 <aezegonz@gmail.com>
2023-11-11 18:28:08 +0000

    fixed: changed order by scores to order by number

--- SQL_introduction/15-groups.sql
+++ SQL_introduction/15-groups.sql
@@ -1,3 +1,3 @@
 -- create a group by score that shows number of same scores
 
-SELECT score, count(score) AS "number" FROM second_table GROUP BY score ORDER BY score DESC
+SELECT score, count(score) AS "number" FROM second_table GROUP BY score ORDER BY number DESC
