a174d55547632ae607e5c603c35af99f1245f862
AaronEGH16 <aezegonz@gmail.com>
2023-11-13 15:26:19 +0000

    fixed database used

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,6 @@
 -- shows all the genres of the show Dexter (through the connection by id with the tv_show_genres table)
 
+USE hbtn_0d_tvshows;
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
