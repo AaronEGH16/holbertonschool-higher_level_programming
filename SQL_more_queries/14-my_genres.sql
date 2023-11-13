17a500dba3d99863e3721d3e0ee1395614aa65b7
AaronEGH16 <aezegonz@gmail.com>
2023-11-13 15:28:40 +0000

    fixed order of show genres

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,8 +1,8 @@
 -- shows all the genres of the show Dexter (through the connection by id with the tv_show_genres table)
 
-USE hbtn_0d_tvshows;
 SELECT tv_genres.name
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
 WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name ASC;
