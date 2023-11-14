b94a52db09c49228ceb0c26fb94ce72dec30893a
AaronEGH16 <aezegonz@gmail.com>
2023-11-14 15:45:24 +0000

    filter table with letter

---
+++ python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+This module contains a program to connect to a database and print all rows.
+When "name" starts with "N"
+"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+
+    db = MySQLdb.connect(host="localhost",
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE name LIKE \"N%\"\
+                ORDER BY id ASC")
+    for row in cur:
+        print(row)
+    cur.close()
+    db.close()
