716503dfa63545821af7a9f080e59b9f06acd5ff
AaronEGH16 <aezegonz@gmail.com>
2023-11-14 16:24:02 +0000

    filter by name

---
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains a program to connect to a database and print all rows.
+When "name" is equal to what the arguments pass
+"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
+
+    cur = db.cursor()
+    cur.execute(f"SELECT * FROM states\
+                WHERE name = \"{state_name_searched}\"\
+                ORDER BY id ASC")
+    for row in cur:
+        print(row)
+    cur.close()
+    db.close()
