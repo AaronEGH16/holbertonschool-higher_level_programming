10abe25ac0f0ace0e48d5d654501bf616b6b7cc3
AaronEGH16 <aezegonz@gmail.com>
2023-11-16 15:59:19 +0000

    same of prev exercise but now is safe of MySQL Injection

---
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This module contains a program to connect to a database and print all rows.
+When "name" is equal to what the arguments pass.
+This code is safe of MySQL Injection.
+"""
+from sys import argv
+import MySQLdb
+
+
+if __name__ == "__main__":
+
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    state_name_searched = argv[4]
+
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=mysql_username,
+                         passwd=mysql_password,
+                         db=database_name)
+
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states\
+                WHERE BINARY name = %s\
+                ORDER BY id ASC", (state_name_searched,))
+    for row in cur:
+        print(row)
+    cur.close()
+    db.close()
